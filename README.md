436467326b6cd106267963cf6d074b219a3fc74f
EllyPirelly <ellypirelly1@yahoo.de>
2021-06-01 11:01:28 +0200

    fix simple cf, add rgba cf, clean-up

--- README.md
+++ README.md
@@ -1,35 +1,30 @@
-# Simple Color Flipper
+# Named Color, Hex Color and rgba Color Flipper
 
 ## What is this about?
 
-A simple color flipper that has two flippable color options to chose from:
+A named color, hex color and rgba color flipper that has 3 flippable color options to chose from:
 
-- simple colors
+- named colors
 - hex colors
+- rgba colors
 
 ### Sources
 
-- starting point was this [FreeCodecamp article - 40 JavaScript courses for Beginners](https://www.freecodecamp.org/news/javascript-projects-for-beginners/)
-- [the color flipper part on FreeCodecamp](https://www.freecodecamp.org/news/javascript-projects-for-beginners/#how-to-create-a-color-flipper)
+- Starting point: [FreeCodecamp article - 40 JavaScript courses for Beginners](https://www.freecodecamp.org/news/javascript-projects-for-beginners/)
+- See this link for [the color flipper part in that article](https://www.freecodecamp.org/news/javascript-projects-for-beginners/#how-to-create-a-color-flipper)
 
-Author of this is John Smilga
+Author/Creator of that color flipper is John Smilga
 
 - [YouTube video color flipper](https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s)
-- [overview of the JavaScript projects, John Smilga created](https://www.vanillajavascriptprojects.com/)
 - [John Smilga's color flipper on GitHub](https://github.com/john-smilga/javascript-basic-projects/tree/master/01-color-flipper)
-
-- [an additional source how to calculate hex colors](https://codepen.io/iamsaief/pen/NWxQMjW)
+- [Overview of the JavaScript projects, John Smilga created](https://www.vanillajavascriptprojects.com/)
+- [An additional source how to calculate hex colors](https://codepen.io/iamsaief/pen/NWxQMjW)
 
 ### Visuals - what you get with this code
 
 ![screen01](./img/screenshot01.png)
 ![screen01](./img/screenshot02.png)
 ![screen01](./img/screenshot03.png)
-
-<!-- 480px
-![480px](./img/screenshots/)
-640px
-![640px](./img/screenshots/) -->
 
 ### Tech
 
--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const colorsSimple = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
+const colorsNamed = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen',];
 const colorsHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-const btnSimple = document.querySelector('[data-btn="btn-simple"]');
+const btnNamed = document.querySelector('[data-btn="btn-simple"]');
 const btnHex = document.querySelector('[data-btn="btn-hex"]');
-
-const btnHero = document.querySelector('[data-btn="btn-hero"]');
+const btnRgba = document.querySelector('[data-btn="btn-rgba"]')
 const colorDisplay = document.querySelector('[data-display="color-display"]');
 
 function getRandomSimpleNumber() {
-    return Math.floor(Math.random() * colorsSimple.length);
+    return Math.floor(Math.random() * colorsNamed.length);
 }
 
-btnSimple.addEventListener('click', function () {
+btnNamed.addEventListener('click', function () {
     const randomSimpleNumber = getRandomSimpleNumber();
-
-    document.body.style.backgroundColor = colorsSimple[randomSimpleNumber];
-    colorDisplay.textContent = colorsSimple[randomSimpleNumber];
+    document.body.style.backgroundColor = colorsNamed[randomSimpleNumber];
+    colorDisplay.textContent = colorsNamed[randomSimpleNumber];
 });
-
-
-/* function getRandomHexNumber() {
-    return Math.floor(Math.random() * colorsHex.length);
-} */
 
 function getRandomHex() {
     const calculateRandomHex = Math.floor(Math.random() * 0xf00000 + 0xfffff).toString(16);
@@ -29,13 +22,26 @@ function getRandomHex() {
 }
 
 btnHex.addEventListener('click', function () {
-
-    /* let hexColor = '#';
-    for (let i = 0; i < 6; i++) {
-        hexColor += colorsHex[getRandomHexNumber()];
-    } */
-
     const randomHex = `#${getRandomHex()}`;
     document.body.style.backgroundColor = randomHex;
     colorDisplay.textContent = randomHex;
+});
+
+btnRgba.addEventListener('click', function () {
+
+    const randomRgbNumber = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const randomAlphaNumber = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.random(min, max).toFixed(1);
+    }
+
+    const r = randomRgbNumber(0, 255);
+    const g = randomRgbNumber(0, 255);
+    const b = randomRgbNumber(0, 255);
+    const a = randomAlphaNumber(0, 1);
+    const rgba = `rgba(${r}, ${g}, ${b}, ${a})`;
+
+    document.body.style.backgroundColor = rgba;
+    colorDisplay.textContent = rgba;
 });
